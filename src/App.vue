797677f1bb6e5506bee2aeb9a9447a869e63209c
shijianzhong <994129509@qq.com>
2024-09-23 10:40:41 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import KeroseneLamp from "./components/KeroseneLamp.vue";
+</script>
+
+<template>
+  <div class="container">
+    <KeroseneLamp></KeroseneLamp>
+  </div>
+</template>
+
+<style scoped>
+.container{
+  height: 90%;
+}
+.logo.vite:hover {
+  filter: drop-shadow(0 0 2em #747bff);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #249b73);
+}
+
+:root {
+  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+
+  color: #0f0f0f;
+  background-color: transparent;
+
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-text-size-adjust: 100%;
+}
+</style>
